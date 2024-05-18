a1e41101b4b9b9ff18bfb7b4b65edc344df3a5e7
olegbaranovskyi <153509142+olegbaranovskyi@users.noreply.github.com>
2024-05-18 19:36:42 +0300

    Auto git push ...

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function App() {
   return (
-    <div className="app">
-      <header className="app-header m-4">
-        <h1>Memorize Mate</h1>
-        This will help you to expand your vocabulary.
-      </header>
-    </div >
+    <Container>
+      <Row className="justify-content-md-center mt-5">
+        <Col md={10} className="text-center">
+          <h1>Memorize Mate</h1>
+          <p>This will help you to expand your vocabulary.</p>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
