517f84625dbf6828b50dce9b98f1032b2f65c072
olegbaranovskyi <153509142+olegbaranovskyi@users.noreply.github.com>
2024-05-18 18:53:11 +0300

    Auto git push ...

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        The memory mate app.
+      </header>
+    </div >
+  );
+}
+
+export default App;
