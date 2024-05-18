a7c5549f2c96a8bb8ab4ff1298b8cbe014206266
olegbaranovskyi <153509142+olegbaranovskyi@users.noreply.github.com>
2024-05-18 19:14:57 +0300

    Auto commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Memorize Mate</h1>
+        This will help you to expand your vocabulary.
       </header>
     </div >
   );
