e5543750dc25694ad0fe5f0e004a7b38e2d77e74
olegbaranovskyi <153509142+olegbaranovskyi@users.noreply.github.com>
2024-05-18 18:59:59 +0300

    Auto git push ...

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        The memory mate app.
+        Memorize mate.
       </header>
     </div >
   );
