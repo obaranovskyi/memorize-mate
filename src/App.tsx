14567173513d0f6863957cf49d69534233c7966c
olegbaranovskyi <153509142+olegbaranovskyi@users.noreply.github.com>
2024-05-18 19:11:59 +0300

    Auto commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Memorize mate. Version #1
+        <h1>Memorize Mate</h1>
       </header>
     </div >
   );
