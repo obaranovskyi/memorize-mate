6b29159ab8b3d7f1b86463cbf3a5531338884efa
olegbaranovskyi <153509142+olegbaranovskyi@users.noreply.github.com>
2024-05-19 10:11:13 +0300

    Auto commit

--- src/router.tsx
+++ src/router.tsx
@@ -1,8 +1,8 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createHashRouter } from "react-router-dom";
 import App from "./App";
 import PhrasesPage from "./Phrases/PhrasesPage";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <App />,
